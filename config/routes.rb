b77b01e55404cd97b999c7ac3e9252eb12f402b7
john ferreira <john@mediasifter.co>
2018-11-05 14:10:53 +0800

    added cocktails controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'cocktails/index'
+  get 'cocktails/show'
+  get 'cocktails/new'
+  get 'cocktails/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
